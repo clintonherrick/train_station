fccb4f8ee2747147c468eb9988ed89a12ad890c9
clinton herrick & Janice Dunn <student@epicodus.com>
2015-08-20 16:42:20 -0700

    Allow user to coordinate city and train stops.

--- app.rb
+++ app.rb
@@ -6,95 +6,152 @@ require('./lib/city')
 require('pg')
 require('pry')
 
-DB = PG.connect({:dbname=> 'train_station_test'})
+DB = PG.connect({:dbname=> 'train_station'})
 
 get("/") do
+  @cities = City.all()
+  @trains = Train.all()
   erb(:index)
 end
 
-
-get("/cities/new") do
-  erb(:city_form)
-end
-
-post('/cities') do
-  location = params.fetch("location")
-  city = City.new({:location => location, :id => nil})
-  city.save()
-  erb(:success)
-end
-
-get('/cities') do
+get("/cities") do
   @cities = City.all()
   erb(:cities)
 end
 
-get('/cities/:id') do
-  @city = City.find(params.fetch('id').to_i())
-  erb(:city)
-end
-
-get("/cities/:id/edit") do
-  @city = City.find(params.fetch("id").to_i())
-  erb(:city_edit)
-end
-
-patch("/cities/:id") do
-  location = params.fetch("location")
-  @city = City.find(params.fetch("id").to_i())
-  @city.update({:location => location})
-  erb(:city)
-end
-
-delete('/cities/:id') do
-  @city = City.find(params.fetch("id").to_i())
-  @city.delete()
-  @cities = City.all()
-  erb(:index)
-end
-
-get("/trains/new") do
-  erb(:train_form)
-end
-
-post('/trains') do
-  color = params.fetch("color")
-  train = Train.new({:color => color, :id => nil})
-  train.save()
-  erb(:success)
-end
-
-get('/trains') do
+get("/trains") do
   @trains = Train.all()
   erb(:trains)
 end
 
-get('/trains/:id') do
-  @train = Train.find(params.fetch('id').to_i())
-  erb(:train)
+post("/cities") do
+  location = params.fetch("location")
+  city = City.new({:location => location, :id => nil})
+  city.save()
+  @cities = City.all()
+  erb(:cities)
 end
 
-get('/trains/:id/edit') do
+post("/trains") do
+  color = params.fetch("color")
+  train = Train.new({:color => color, :id => nil})
+  train.save()
+  @trains = Train.all()
+  erb(:trains)
+end
+
+get("/cities/:id") do
+  @city = City.find(params.fetch("id").to_i())
+  @trains = Train.all()
+  erb(:city_info)
+end
+
+get("/trains/:id") do
   @train = Train.find(params.fetch("id").to_i())
-  erb(:train_edit)
+  @cities = City.all()
+  erb(:train_info)
+end
+
+patch("/cities/:id") do
+  city_id = params.fetch("id").to_i()
+  @city = City.find(city_id)
+  train_ids = params.fetch("train_ids")
+  @city.update({:train_ids => train_ids})
+  @trains = Train.all()
+  erb(:city_info)
 end
 
 patch("/trains/:id") do
-  color = params.fetch("color")
-  @train = Train.find(params.fetch("id").to_i())
-  @train.update({:color => color})
-  erb(:train)
+  train_id = params.fetch("id").to_i()
+  @train = Train.find(train_id)
+  city_ids = params.fetch("city_ids")
+  @train.update({:city_ids => city_ids})
+  @cities = City.all()
+  erb(:train_info)
 end
 
-delete('/trains/:id') do
-  @train = Train.find(params.fetch("id").to_i())
-  @train.delete()
-  @trains = Train.all()
-  erb(:index)
-end
-
-
-
+# get("/cities/new") do
+#   erb(:city_form)
+# end
+#
+# post('/cities') do
+#   location = params.fetch("location")
+#   city = City.new({:location => location, :id => nil})
+#   city.save()
+#   erb(:success)
+# end
+#
+# get('/cities') do
+#   @cities = City.all()
+#   erb(:cities)
+# end
+#
+# get('/cities/:id') do
+#   @city = City.find(params.fetch('id').to_i())
+#   erb(:city)
+# end
+#
+# get("/cities/:id/edit") do
+#   @city = City.find(params.fetch("id").to_i())
+#   erb(:city_edit)
+# end
+#
+# patch("/cities/:id") do
+#   location = params.fetch("location")
+#   @city = City.find(params.fetch("id").to_i())
+#   @city.update({:location => location})
+#   erb(:city)
+# end
+#
+# delete('/cities/:id') do
+#   @city = City.find(params.fetch("id").to_i())
+#   @city.delete()
+#   @cities = City.all()
+#   erb(:index)
+# end
+#
+# get("/trains/new") do
+#   erb(:train_form)
+# end
+#
+# post('/trains') do
+#   color = params.fetch("color")
+#   train = Train.new({:color => color, :id => nil})
+#   train.save()
+#   erb(:success)
+# end
+#
+# get('/trains') do
+#   @trains = Train.all()
+#   erb(:trains)
+# end
+#
+# get('/trains/:id') do
+#   @train = Train.find(params.fetch('id').to_i())
+#   erb(:train)
+# end
+#
+# get('/trains/:id/edit') do
+#   @train = Train.find(params.fetch("id").to_i())
+#   erb(:train_edit)
+# end
+#
+# patch("/trains/:id") do
+#   color = params.fetch("color")
+#   @train = Train.find(params.fetch("id").to_i())
+#   @train.update({:color => color})
+#   erb(:train)
+# end
+#
+# delete('/trains/:id') do
+#   @train = Train.find(params.fetch("id").to_i())
+#   @train.delete()
+#   @trains = Train.all()
+#   erb(:index)
+# end
+#
+#
+#
 
 # post("/tasks") do
 #   description = params.fetch("description")
