6dcbd89582664740d7b0fc015a64ca80516d56b8
clinton herrick & Janice Dunn <student@epicodus.com>
2015-08-20 10:45:34 -0700

    adds intergration spec for return home for cities and trains

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -28,7 +28,34 @@ end
 describe('return to home page', {:type => :feature}) do
   it('allows a user to return to the home page') do
     visit('/trains')
-    # binding.pry
+    click_link('Return Home')
+    expect(page).to have_content('Welcome to the Train Station!')
+  end
+end
+
+describe('adding a new city', {:type => :feature}) do
+  it('allows a user to add a new city to the list of cities') do
+    visit('/')
+    click_link('Add New City')
+    fill_in('location', :with => 'Paris' )
+    click_button('Add City')
+    expect(page).to have_content('Success')
+  end
+end
+
+describe('viewing all of the cities', {:type => :feature}) do
+  it('allows a user to see all of the cities that have been listed') do
+    city = City.new({:location => 'Paris', :id => nil})
+    city.save()
+    visit('/')
+    click_link('View All Cities')
+    expect(page).to have_content("Your Current Cities")
+  end
+end
+
+describe('return to home page', {:type => :feature}) do
+  it('allows a user to return to the home page') do
+    visit('/cities')
     click_link('Return Home')
     expect(page).to have_content('Welcome to the Train Station!')
   end
